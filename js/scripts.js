59284e4aee2c2c446c9502488e570f0e0fc51274
shruti2286 <47882761+shruti2286@users.noreply.github.com>
2020-02-27 12:08:54 -0500

    GEOJSON ADDED

--- js/scripts.js
+++ js/scripts.js
@@ -103,7 +103,7 @@ map.on('style.load', function() {
   // add a geojson source to the map using our external geojson file
   map.addSource('pluto-bk-cd6', {
     type: 'geojson',
-    data: './data/brooklyn_cd_6_trimmed.geojson',
+    data: './data/CB104.geojson',
   });
 
   // let's make sure the source got added by logging the current map state to the console
